5e60844d43167e39d2048e80566897193987e191
“yourname” <82037182@qq.com>
2021-06-15 14:06:20 +0800

    第24课 - 第三方库的使用支持

--- code/pro-rule.mk
+++ code/pro-rule.mk
@@ -5,7 +5,8 @@ DIR_PROJECT := $(realpath .)
 DIR_BUILD_SUB := $(addprefix $(DIR_BUILD)/, $(MODULES))
 MODULE_LIB := $(addsuffix .a, $(MODULES))
 MODULE_LIB := $(addprefix $(DIR_BUILD)/, $(MODULE_LIB))
-
+EXTERNAL_LIB := $(wildcard $(DIR_LIBS_LIB)/*)
+EXTERNAL_LIB := $(patsubst $(DIR_LIBS_LIB)/%, $(DIR_BUILD)/%, $(EXTERNAL_LIB))
 
 APP := $(addprefix $(DIR_BUILD)/, $(APP))
 
@@ -15,6 +16,7 @@ define makemodule
 			DEBUG:=$(DEBUG) \
 			DIR_BUILD:=$(addprefix $(DIR_PROJECT)/, $(DIR_BUILD)) \
 			DIR_COMMON_INC:=$(addprefix $(DIR_PROJECT)/, $(DIR_COMMON_INC)) \
+			DIR_LIBS_INC:=$(addprefix $(DIR_PROJECT)/, $(DIR_LIBS_INC)) \
 			CMD_CFG:=$(addprefix $(DIR_PROJECT)/, $(CMD_CFG)) \
 			MOD_CFG:=$(addprefix $(DIR_PROJECT)/, $(MOD_CFG)) \
 			MOD_RULE:=$(addprefix $(DIR_PROJECT)/, $(MOD_RULE)) && \
@@ -33,10 +35,13 @@ compile : $(DIR_BUILD) $(DIR_BUILD_SUB)
 	done
 	@echo "Compile Success!"
 	
-link $(APP) : $(MODULE_LIB)
+link $(APP) : $(MODULE_LIB) $(EXTERNAL_LIB)
 	@echo "Begin to link ..."
 	$(CC) -o $(APP) -Xlinker "-(" $^ -Xlinker "-)" $(LFLAGS)
 	@echo "Link Success!"
+	
+$(DIR_BUILD)/% : $(DIR_LIBS_LIB)/%
+	$(CP) $^ $@
 	
 $(DIR_BUILD) $(DIR_BUILD_SUB) : 
 	$(MKDIR) $@
